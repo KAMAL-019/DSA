a48088488ed860fa1ece34616548dd8c5255f49f
KAMAL-019 <107632486+KAMAL-019@users.noreply.github.com>
2022-09-21 22:55:53 +0530

    Create inputoutput.cpp

---
+++ Assignment/Assignment-1/inputoutput.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+int a  ,b ,c,sum = 0;
+
+cin>>a>>b>>c;
+sum = a+b+c;
+cout<<sum; 
+    return 0;
+}
+link - https://www.hackerrank.com/challenges/cpp-input-and-output/submissions/code/289717828
