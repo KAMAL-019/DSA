3560bef7ee996828239a8797fd202f01fa28bef5
KAMAL-019 <107632486+KAMAL-019@users.noreply.github.com>
2022-09-21 22:57:36 +0530

    Create datatype.cpp

---
+++ Assignment/Assignment-1/datatype.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+   
+   int a;
+    long b ; 
+    char c ;
+    float d;
+    double e;
+
+     cin >> a >> b >> c >> d >> e;
+    //  cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e<<endl;
+     printf("%d\n%ld\n%c\n%f\n%lf",a,b,c,d,e);
+
+    return 0;
+    // return 0;
+
+}
+link - https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/submissions/code/289720530
