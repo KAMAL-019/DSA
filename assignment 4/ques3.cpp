1d13b2d84db4779d1d9d2cb84846f876820a4182
KAMAL-019 <107632486+KAMAL-019@users.noreply.github.com>
2023-01-30 21:23:58 +0530

    Create ques3.cpp

---
+++ assignment 4/ques3.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int arr1[a];
+    int sum = 0;
+    int count = 0;
+    for(int i=0;i<a;i++)
+    {
+        cin>>arr1[i];
+    }
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<a;j++)
+        {
+            sum=arr1[i]+arr1[j];
+            if(i!=j)
+            {
+                if(sum%b==0 && sum>=b)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    count=count/2;
+    cout<<count<<endl;
+    return 0;
+}
